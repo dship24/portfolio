7d85251d3a5d5ccb6fec3195ce395cea325bc88c
dship24 <87671216+dship24@users.noreply.github.com>
2021-09-09 14:45:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("mySidebar").style.width = "20%";
+  document.getElementById("main").style.marginLeft = "20%";
 }
 
 function closeNav() {
