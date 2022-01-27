44e2a7705bfaef1cb30d8ba501eb537189da3519
dship24 <87671216+dship24@users.noreply.github.com>
2022-01-27 10:05:23 -0600

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-  document.getElementById("mySidebar").style.width = "20%";
-  document.getElementById("main").style.marginLeft = "20%";
-}
-
-function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
-}
